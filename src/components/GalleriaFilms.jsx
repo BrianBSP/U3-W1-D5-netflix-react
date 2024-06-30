0dcdfdbabd45c746f12354834e7b7b1b1310c014
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-07-01 01:07:23 +0200

    aggiunte ultime modifiche

--- src/components/GalleriaFilms.jsx
+++ src/components/GalleriaFilms.jsx
@@ -8,6 +8,7 @@ class GalleriaFilms extends Component {
     return (
       <Container fluid className="px-0">
         <MoviesRow title="Trending Now" cerca="avengers" />
+        <MoviesRow title="Best Movies" cerca="Guardians of the Galaxy" />
         <MoviesRow title="Watch It Again" cerca="harry potter" />
         <MoviesRow title="New Releases" cerca="pirates of the caribbean" />
         <MoviesRow title="Fantasy" cerca="lord of the rings" />
